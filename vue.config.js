ae975e716739d3b373b3ce79411b7a75aa2c4bfb
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-10-08 05:54:50 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,13 @@
-module.exports = {
-    publicPath: '',
-    devServer: {
-        public: process.env.CORDOVA_PLATFORM ? process.env.DEVSERVER_PUBLIC : ""
-    },
-    pluginOptions: {
-        cordovaPath: 'src-cordova'
-    },
-    chainWebpack: config => {
-        config.module
-            .rule('ts')
-            .use("ts-loader")
-            .loader("ts-loader")
-            .tap(options => {
-                options.compiler = "ttypescript";
-                return options
-        });
-
-        config.module
-            .rule('tsx')
-            .use("ts-loader")
-            .loader("ts-loader")
-            .tap(options => {
-                options.compiler = "ttypescript";
-                return options
-            });
-    }
+module.exports = {publicPath: 'devServer:
+	{ public: process.env.CORDOVA_PLATFORM 
+	? process.env.DEVSERVER_PUBLIC : 
+	""}, pluginOptions: 
+{cordovaPath: 
+'src-cordova' }  chainWebpack: config = > 
+	{config.module.rule('ts')
+  .use("ts-loader").loader("ts-loader")
+.tap(options => {options.compiler = 
+	"ttypescript";.ts_return options }); 
+	 config.module.rule('tsx').use("ts-loader").loader("ts-loader")
+.tap(options =  >{options.compiler = "ttypescript"; return option});
 };
-
-
